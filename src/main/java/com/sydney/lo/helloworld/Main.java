0de27fa6f269ade959dbeaa7a1b8264a1c620596
Sydney Lo <sydneyvlo@gmail.com>
2017-10-22 14:57:04 -0700

    Boilerplate Maven project that prints out Hello world.

---
+++ src/main/java/com/sydney/lo/helloworld/Main.java
@@ -0,0 +1,12 @@
+package com.sydney.lo.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
